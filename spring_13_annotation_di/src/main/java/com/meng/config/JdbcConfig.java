cd931f8d360f8caa534bede39575125a089ce02c
小胡 <1589029587@qq.com>
2022-05-09 17:21:30 +0800

    update

--- spring_13_annotation_di/src/main/java/com/meng/config/JdbcConfig.java
+++ spring_13_annotation_di/src/main/java/com/meng/config/JdbcConfig.java
@@ -4,7 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.meng.service.BookService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
